ee5ead488341040f11b160f713cdb4f5f534fff5
elboffin <olamide.adetula.1744@gmail.com>
2023-02-27 20:02:39 +0100

    goerli deployment fix

--- Nftmarketplace/migrations/2_deploy_contracts.js
+++ Nftmarketplace/migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ const NFTMarketplace = artifacts.require("NFTMarketplace");
 module.exports = async (deployer) => {
     const accounts = await web3.eth.getAccounts()
   
-    await deployer.deploy(NFTMarketplace, 'ZeeNFT', 'ZTH', accounts[1])
+    await deployer.deploy(NFTMarketplace, 'ZeeNFT', 'ZTH', accounts[0])
   }
